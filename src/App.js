81f177a7021d3a86e13886dc8619fadf69244841
Topher H <chris@Chris.local>
2017-04-29 11:25:29 -0700

    Completed Sprint 0. Added AirBnb EsLint & created Books Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Books from './components/Books.js';
+import './css/App.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div
+          className="app-bar"
+        >
+          <div> Story Time </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>My Books</h2>
+          <ul>
+            <Books />
+          </ul>
       </div>
     );
   }
---
+++ src/components/Books.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Books extends Component {
+  render(){
+    return(
+      <li> The Sun Also Rises by Ernest Hemingway </li>
+    )
+  }
+
+}
+
+export default Books;
